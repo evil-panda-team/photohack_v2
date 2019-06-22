cae0b7f460f6cb4d0fd412adfa7c13cd9380ba88
vladostan <vladostan94@gmail.com>
2019-06-22 14:18:23 +0000

    Rewritten code for simpler arguments

---
+++ icface/run.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 16:54:36 2019
+
+@author: kenny
+"""
+
+#from imutils.face_utils import FaceAligner
+from imutils.face_utils import rect_to_bb
+import imutils
+import dlib
+import cv2
+#from imutils import face_utils
+import numpy as np
+from PIL import Image
+#import pdb   
+import argparse
+
+import os
+from options.test_options import TestOptions
+from data.data_loader import CreateDataLoader
+from models.models import create_model
+#from util.visualizer import Visualizer
+#from util import html
+import pdb
+
+parser = argparse.ArgumentParser(description='Process some image input.')
+parser.add_argument('--img', action='store', help='Image to parse')
+args = parser.parse_args()
+
+###### PART 1
+
+#def pre_process(path):
+    
+detector = dlib.get_frontal_face_detector()
+
+name = args.img
+cap = cv2.imread(name) # add your image here
+
+image= cv2.resize(cap, (400, 400))
+
+RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+   
+rects = detector(RGB, 1)
+faces = dlib.full_object_detections()
+
+for rect in rects:
+    c1=rect.dcenter()
+    (x, y, w, h) = rect_to_bb(rect)
+    w=np.int(w*1.6) 
+    h=np.int(h*1.6) 
+    x=c1.x-np.int(w/2.0)
+    y=c1.y-np.int(h/2.0)
+    if y<0:
+       y=0
+    if x<0:
+       x=0
+       
+    faceOrig = imutils.resize(RGB[y:y+h, x:x+w], height=256) #y=10,h+60,W+40
+      
+    d_num = np.asarray(faceOrig)
+    
+    f_im = Image.fromarray(d_num)
+    
+    f_im.save('./new_crop/'+ name[4:-4] +'.png')
+        
+    
+#### PART 2
+opt = TestOptions().parse()
+opt.nThreads = 1   # test code only supports nThreads = 1
+opt.batchSize = 1  # test code only supports batchSize = 1
+opt.serial_batches = True  # no shuffle
+opt.no_flip = True  # no flip
+
+data_loader = CreateDataLoader(opt)
+dataset = data_loader.load_data()
+model = create_model(opt)
+#visualizer = Visualizer(opt)
+# create website
+web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
+#webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
+# test
+for i, data in enumerate(dataset):
+#    pdb.set_trace()
+    
+  
+    if i >= opt.how_many:
+        break
+    
+    model.set_input(data)
+    img_path = model.get_image_paths()
+    model.test()
+
+    img_path = model.get_image_paths()
+#    print('%04d: process image... %s' % (i, img_path))
+print("Done!")
---
+++ icface/run.sh
@@ -0,0 +1,4 @@
+python3 test.py  \
+--which_ref ./new_crop/rafa.png  \
+--csv_path ./csv/navalniy.csv
+
