4d45a6a78c83d3fae904188a827da50e97a09b75
vladostan <vladostan94@gmail.com>
2019-06-23 14:07:04 +0000

    Added tyanka

--- icface/runner.py
+++ icface/runner.py
@@ -3,6 +3,6 @@
 from run_vlad import create_mp4
 
 if __name__ == "__main__":
-    input_img_path = 'img/gosling.jpg'
-    csv_path = 'csv/nixelpixel.csv'
+    input_img_path = 'img/tyanka.jpg'
+    csv_path = 'csv/clarke_short.csv'
     create_mp4(input_img_path, csv_path)
