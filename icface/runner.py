0744cc2df0533a9fb69842cbe9270fb6ac9a6800
vladostan <vladostan94@gmail.com>
2019-06-23 12:17:59 +0000

    Added clark short

--- icface/runner.py
+++ icface/runner.py
@@ -3,6 +3,6 @@
 from run_vlad import create_mp4
 
 if __name__ == "__main__":
-    input_img_path = 'img/buzova.jpg'
-    csv_path = 'csv/vlad_scream.csv'
+    input_img_path = 'img/gosling.jpg'
+    csv_path = 'csv/nixelpixel.csv'
     create_mp4(input_img_path, csv_path)
