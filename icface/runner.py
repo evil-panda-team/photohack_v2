bcb21d9ce346f81071ebcc0a3c52d007d63e5ff4
vladostan <vladostan94@gmail.com>
2019-06-23 07:31:32 +0000

    Fixed resizing

--- icface/runner.py
+++ icface/runner.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from run import create_mp4
+from run_vlad import create_mp4
 
 if __name__ == "__main__":
-    input_img_path = 'img/rafa.jpg'
+    input_img_path = 'img/vindiesel.jpg'
     csv_path = 'csv/carrey.csv'
     create_mp4(input_img_path, csv_path)
